ec28827d4d1ed4cb18755b367edcff1f62391736
shuent <shunshun.43@gmail.com>
2021-04-11 19:37:00 +0900

    firebase congig to env

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const fireConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+}
+
+firebase.initializeApp(fireConfig)
+
+const auth = firebase.auth()
+const firedb = firebase.firestore()
+export { firebase, auth, firedb }
